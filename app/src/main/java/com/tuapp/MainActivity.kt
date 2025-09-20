f223b86309457b8f687365ce0fa822f9ee43a9dd
gabrielesp2025-arch <gabrielesp2025@gmail.com>
2025-09-20 15:29:06 +0200

    Update MainActivity.kt

--- app/src/main/java/com/tuapp/MainActivity.kt
+++ app/src/main/java/com/tuapp/MainActivity.kt
@@ -3,20 +3,13 @@ package com.tuapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import com.tuapp.ui.NavRoot
+import com.tuapp.ui.NavRoot   // 👈 aquí conectamos NavRoot
 
-@OptIn(ExperimentalMaterial3Api::class)
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { App() }
+        setContent {
+            NavRoot()   // 👈 aquí usamos NavRoot
+        }
     }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun App() {
-    MaterialTheme { NavRoot() }
 }
