42f488a624ef2563c3fc3b2bc3117997852ea52e
gabrielesp2025-arch <gabrielesp2025@gmail.com>
2025-09-20 16:50:08 +0200

    Create ImageUtils.kt

---
+++ src/main/java/com/tuapp/util/ImageUtils.kt
@@ -0,0 +1,17 @@
+package com.tuapp.util
+
+import android.content.Context
+import android.net.Uri
+import androidx.core.content.FileProvider
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun createImageFile(context: Context, prefix: String = "IMG"): File {
+    val time = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+    val name = "${prefix}_$time.jpg"
+    return File(context.filesDir, name) // se guarda dentro de la app
+}
+
+fun uriForFile(context: Context, file: File): Uri =
+    FileProvider.getUriForFile(context, "com.tuapp.fileprovider", file)
