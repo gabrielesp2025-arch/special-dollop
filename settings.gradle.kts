2ac0ff8b61ab946dd2779fb9e9cca4841ba933e5
gabrielesp2025-arch <gabrielesp2025@gmail.com>
2025-09-20 03:43:52 +0200

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "special-dollop" // pon aquí el nombre de tu proyecto si quieres
+include(":app")
