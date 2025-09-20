8c7ae07b6cf557dab71f6b1b1b54a0e693206dbd
gabrielesp2025-arch <gabrielesp2025@gmail.com>
2025-09-20 10:30:10 +0200

    Create NavRoot.kt

---
+++ app/src/main/java/com/tuapp/ui/NavRoot.kt
@@ -0,0 +1,28 @@
+package com.tuapp.ui
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavRoot() {
+    val nav = rememberNavController()
+    NavHost(navController = nav, startDestination = "orders") {
+        composable("orders") {
+            OrdersScreen(
+                onNewOrder = { id -> nav.navigate("order/$id") },
+                onOpen = { id -> nav.navigate("order/$id") },
+                onOpenPrices = { nav.navigate("prices") }
+            )
+        }
+        composable("order/{id}") {
+            OrderDetailScreen(onBack = { nav.popBackStack() })
+        }
+        composable("prices") {
+            PriceReferenceScreen(onBack = { nav.popBackStack() })
+        }
+    }
+}
