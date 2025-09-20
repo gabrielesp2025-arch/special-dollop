a81fb2e3346202705105cf4db831289600209c7a
gabrielesp2025-arch <gabrielesp2025@gmail.com>
2025-09-20 16:00:27 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.tuapp"
-    compileSdk = 34  // 👈 NECESARIO
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.tuapp"
@@ -18,25 +18,8 @@ android {
         vectorDrawables { useSupportLibrary = true }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions { jvmTarget = "17" }
-
     buildFeatures { compose = true }
     composeOptions { kotlinCompilerExtensionVersion = "1.5.14" }
-
     packaging { resources { excludes += "/META-INF/{AL2.0,LGPL2.1}" } }
 }
 
