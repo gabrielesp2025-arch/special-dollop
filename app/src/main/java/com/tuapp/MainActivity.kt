36a419e1b559d80412d9df6a30a49ee602db8b1d
gabrielesp2025-arch <gabrielesp2025@gmail.com>
2025-09-20 15:30:05 +0200

    Update MainActivity.kt

--- app/src/main/java/com/tuapp/MainActivity.kt
+++ app/src/main/java/com/tuapp/MainActivity.kt
@@ -3,13 +3,13 @@ package com.tuapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.tuapp.ui.NavRoot   // 👈 aquí conectamos NavRoot
+import com.tuapp.ui.NavRoot   // 👈 Importamos NavRoot desde la carpeta ui
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            NavRoot()   // 👈 aquí usamos NavRoot
+            NavRoot()   // 👈 Aquí cargamos la pantalla de navegación
         }
     }
 }
